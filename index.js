cfd61bc38135e5b5590f96385d1ab9a3c7342ad0
SimonLeis <simse.leis@web.de>
2023-10-23 21:07:23 +0200

    added visual interface
    added the function which creates the squares with the given number and
    lets it start with 16 by 16

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  let numberOfSquares = 16 * 16;
+
+  const createGrid = (numberOfSquares) => {
+    for (let index = 0; index < numberOfSquares; index++) {
+      const div = document.createElement("div");
+      div.classList.add("square");
+      div.style.width = `${1000 / Math.sqrt(numberOfSquares) - 2}px`;
+      div.style.height = `${1000 / Math.sqrt(numberOfSquares) - 2}px`;
+
+      console.log(div.style.width);
+
+      container.appendChild(div);
+    }
+  };
+
+  const destroyGrid = () => {
+    document.querySelectorAll(".square").forEach((element) => {
+      element.parentNode.removeChild(document.querySelector(".square"));
+    });
+  };
+
+  createGrid(numberOfSquares);
+});
